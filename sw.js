02c1869bd69b84090996c179fb2046b274e9c9b6
Eason-988 <easonto327@gmail.com>
2026-07-23 14:02:03 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'stockfish-gui-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './stockfish-18-single-lite.js',
+  './stockfish-18-single-lite.wasm',
+  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css',
+  'https://code.jquery.com/jquery-3.6.0.min.js',
+  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/chess.js/0.10.3/chess.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
